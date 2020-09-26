0543c2b014d1788192df5f2f0855617a735a97db
Anton Bagdatyev (Tonix) <antonytuft@gmail.com>
2020-09-26 10:48:17 +0200

    Increased default concurrent option up to 1024 spawn processes

--- Gruntfile.sample.js
+++ Gruntfile.sample.js
@@ -2,29 +2,32 @@
  * Grunt Hub Automator.
  *
  * This file is a sample.
- * 
+ *
  * Copy this file and save it in the same directory with the name Gruntfile.js.
- * 
+ *
  * Edit the hub.all.src to point to your Gruntfile locations (your Grunt projects).
- * 
+ *
  * Then create a grunt-hub-automator daemon script that will run the Grunt Hub Automator
  * (use the provided grunt-hub-automator.sample).
- * 
+ *
  * Start the grunt-hub-automator daemon script and you're done.
  */
-module.exports = function(grunt) {
-  'use strict';
+module.exports = function (grunt) {
+  "use strict";
 
   grunt.initConfig({
     hub: {
+      options: {
+        concurrent: 1024,
+      },
       all: {
-        src: ['/path/to/*/Gruntfile.js']
+        src: ["/path/to/*/Gruntfile.js"],
         //, tasks: ['yourtask'] // Uncomment this line and add the tasks to execute for each project found by Grunt Hub Automator.
-      }
-    }
+      },
+    },
   });
 
-  grunt.loadNpmTasks('grunt-hub');
+  grunt.loadNpmTasks("grunt-hub");
 
-  grunt.registerTask('default', ['hub']);
+  grunt.registerTask("default", ["hub"]);
 };
